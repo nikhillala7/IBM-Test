8e1cf400d7f0196d320da17c81e774d0402c2d19
Nikhil lala <nikhillala0106@gmail.com>
2019-10-11 13:34:27 -0400

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+  	for(int i = 1 ; i < 100 ; i ++){
+  		if(i%15 == 0 )
+  			System.out.println("FizzBuzz");
+  		if(i%3 == 0 )
+  			System.out.print("Fizz");
+  		if(i%5==0)
+  			Syste.out.println("Buzz");
+  	}
+  }
+}
